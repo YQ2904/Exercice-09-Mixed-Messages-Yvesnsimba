47df01f3b838bb6d458901430be659bb81f48da5
Yves Nsimba <yvesnsimba29@gmail.com>
2023-07-27 11:57:23 +0000

    add function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-function 
+function generateRandomMessage() {
+  var messages = [
+    "Salut, tu vas bien?",
+    "Heureux de te revoir!",
+    "Bienvenu!",
+    "Hello!"
+  ];
+  var randomMessage = messages[Math.floor(Mathrandom() * messages.length)]
+  return randomMessage;
+}
+
+console.log(RandomMessage);
